91707f5ef57e9835af7f42f858e6d116b3cd17eb
SlavaOST <slavaost-it@mail.ru>
2023-05-11 19:55:20 +0300

    feat: change styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import s from "./App.module.scss";
+import React, {useEffect} from 'react';
 import {AppRoutes} from "../utils/routes/AppRoutes";
 import {Header} from "../features/header/Header";
 import {Footer} from "../features/footer/Footer";
 import {Sidebar} from '../features/sidebar/Sidebar';
+import {useAppDispatch} from "../utils/hooks/hooks";
+import {getAllCategoriesTC} from "../bll/reducers/categories-reducer";
+import {getAllProductsTC} from "../bll/reducers/products-reducer";
 
 
 export const App = () => {
+    const dispatch = useAppDispatch()
+
+
+    useEffect(() => {
+        dispatch(getAllCategoriesTC())
+        dispatch(getAllProductsTC())
+    }, [dispatch])
+
     return (
-        <div className={s.app}>
+        <div className="app">
             <Header/>
 
-            <div className={s.container}>
+            <div className="container">
                 <Sidebar/>
                 <AppRoutes/>
             </div>
 
             <Footer/>
         </div>
-
     );
 }
