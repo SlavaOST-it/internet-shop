a09361d48bd570f2395ab50e5abb82f2e798e3bc
SlavaOST <slavaost-it@mail.ru>
2023-06-25 20:00:46 +0300

    feat: add saga middleware

--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,15 +1,19 @@
 import {combineReducers} from "redux";
+import createSagaMiddleware from "redux-saga";
 import {configureStore} from "@reduxjs/toolkit";
 import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
 
 import {appReducer} from "../reducers/app-reducer";
-import {productsReducer} from "../reducers/products-reducer";
-import {categoriesReducer} from "../reducers/categories-reducer";
+import {productsReducer, productsWatcherSaga} from "../reducers/products-reducer";
+import {categoriesReducer, categoryWatcherSaga} from "../reducers/categories-reducer";
 
 import {AppReducerActionTypes} from "../reducers/reducersTypes/appReducer-types";
 import {ProductsReducerActionTypes} from "../reducers/reducersTypes/productsReducer-types";
 import {CategoriesReducerActionTypes} from "../reducers/reducersTypes/categoriesReducer-types";
 
+
+
+type ReduxActionType = AppReducerActionTypes | CategoriesReducerActionTypes | ProductsReducerActionTypes
 
 const rootReducer = combineReducers({
     app: appReducer,
@@ -17,13 +21,23 @@ const rootReducer = combineReducers({
     categories: categoriesReducer,
 })
 
-type ReduxActionType = AppReducerActionTypes | CategoriesReducerActionTypes | ProductsReducerActionTypes
+// ============= SAGA ======================//
+const sagaMiddleware = createSagaMiddleware()
+
+function* rootWatcher() {
+    yield categoryWatcherSaga()
+    yield productsWatcherSaga()
+}
 
 
+// ============= Store ======================//
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunkMiddleware, sagaMiddleware)
 })
+
+// then run the saga
+sagaMiddleware.run(rootWatcher)
 
 export type RootState = ReturnType<typeof store.getState>
 
