c0a5f89f9b01dce3a6f2d9c3d04d56a296e209af
SlavaOST <slavaost-it@mail.ru>
2023-06-25 20:01:52 +0300

    feat: add types response

--- src/api/categoriesAPI.ts
+++ src/api/categoriesAPI.ts
@@ -1,9 +1,10 @@
 import {instance} from "./apiConfig/instance";
 import {AllCategories, Category} from "./apiConfig/typesAPI/categoriesAPI-types";
+import {AxiosResponse} from "axios";
 
 
-export const categoriesAPI = {
-    getAllCategories() {
+export const categoriesAPI  = {
+    getAllCategories(): Promise<AxiosResponse<Category[]>> {
         return instance.get<Category[]>(`/categories`)
     },
 
--- src/api/productsAPI.ts
+++ src/api/productsAPI.ts
@@ -1,9 +1,10 @@
 import {instance} from "./apiConfig/instance";
 import {ProductListType} from "./apiConfig/typesAPI/productsAPI-types";
+import {AxiosResponse} from "axios/index";
 
 
 export const productsAPI = {
-    getAllProducts() {
+    getAllProducts(): Promise<AxiosResponse<ProductListType[]>> {
         return instance.get<ProductListType[]>(`/products`)
     },
 }
